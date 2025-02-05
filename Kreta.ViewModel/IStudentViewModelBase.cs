00e64732cb5760874253e2048a2d77b938c03526
Krisztin Benedek <krisztinbeni@gmail.com>
2025-02-05 10:22:56 +0100

    Működik a adatok lekérése backendről

---
+++ Kreta.ViewModel/IStudentViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using Kreta.Shared.Models;
+using Kreta.ViewModelProject.Base;
+using System.Collections.ObjectModel;
+
+namespace Kreta.ViewModelProject
+{
+    public interface IStudentViewModelBase: IBaseViewModel
+    {
+        public ObservableCollection<Student> Students { get; set; }
+    }
+}
---
+++ KretaBlazorWeb/Kreta/Kreta/Components/Pages/SchoolCitizens/StudentPage.razor.cs
@@ -0,0 +1,19 @@
+﻿using Kreta.ViewModelProject;
+using Microsoft.AspNetCore.Components;
+
+namespace Kreta.Components.Pages.SchoolCitizens
+{
+    public partial class StudentPage
+    {
+        [Inject] IStudentViewModelBase? StudentViewModel { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (StudentViewModel != null)
+            {
+                await StudentViewModel.LoadDataAsync();
+            }
+            await base.OnInitializedAsync();
+        }
+    }
+}
